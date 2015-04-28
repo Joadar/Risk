9a7f094ec8ffece1f2abf767decba5316643e50f
Bryan Reymonenq <bryan.reymonenq@gmail.com>
2015-04-28 19:25:55 +0200

    Ajout de commentaires et de valeurs par défaut sur la classe Unit

--- core/src/fr/fliizweb/risk/Class/Infantry.java
+++ core/src/fr/fliizweb/risk/Class/Infantry.java
@@ -7,5 +7,4 @@ public class Infantry extends Unit {
     protected int value = 1;
     protected int attack = 1;
     protected int def = 1;
-
 }
--- core/src/fr/fliizweb/risk/Class/Unit.java
+++ core/src/fr/fliizweb/risk/Class/Unit.java
@@ -5,4 +5,25 @@ package fr.fliizweb.risk.Class;
  */
 public class Unit {
 
+    /**
+     * Variable : value
+     * Type : int
+     * Description : valeur de l'unité en terme de "poids" ou points
+     */
+    protected int value = 1;
+
+    /**
+     * Variable : attacke
+     * Type : int
+     * Description : valeur de l'unité en attaque
+     */
+    protected int attack = 1;
+
+    /**
+     * Variable : attacke
+     * Type : int
+     * Description : valeur de l'unité en défense
+     */
+    protected int def = 1;
+
 }
