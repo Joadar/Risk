e0f881ee613016b4253877bd276014a1ac0a8c2f
Joadar <jonathan.pannetier@gmail.com>
2015-05-09 18:26:54 +0200

    Formulaire Gestion du formulaire de déplacement de troupes. Problème au niveau du tap.

--- core/src/fr/fliizweb/risk/Screens/GameScreen.java
+++ core/src/fr/fliizweb/risk/Screens/GameScreen.java
@@ -15,7 +15,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -50,6 +49,7 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
     InputMultiplexer inputMultiplexer;
 
     private float origDistance, baseDistance, origZoom;
+    private boolean showForm = false, validForm = false;
 
     Map map;
     ArrayList<Player> players;
@@ -81,8 +81,6 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
         stage = new Stage( vp );
         stage.getViewport().setCamera(camera);
 
-        Gdx.app.log("GameScreen", "Camera = " + camera.toString() + " || Width Screen = " + Gdx.graphics.getWidth() + " Height Screen = " + Gdx.graphics.getHeight());
-
         batch = new SpriteBatch();
 
         for(int i = 0; i < map.getZones().size(); i++) {
@@ -107,7 +105,7 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
 
 
                     //Si aucune zone n'est selectionnée
-                    if (!map.isZoneSelected()) {
+                    if (!map.isZoneSelected() && !showForm) {
                         zone.setSelected(true);
                         map.setZoneSelected(zone.getID());
                         for (int i = 0; i < stageActors.size; i++) {
@@ -120,7 +118,7 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
                             }
                         }
                     } else { //Dans le cas où une zone est selectionnée
-                        if (zone.getID() == map.getZoneSelected()) { //Si la zone tapée est la même que celle selectionnée
+                        if (zone.getID() == map.getZoneSelected() && !showForm) { //Si la zone tapée est la même que celle selectionnée
                             //On désélectionne toutes les zones actives
                             map.setZoneSelected(0);
                             zone.setSelected(false);
@@ -137,8 +135,10 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
                         } else if (zone.isActive()) { //Si la zone selectionnée est une zone active
                             //On met la zone tapée de la couleur de la zone selectionnée au préalable.
                             //Puis on désactive toutes les zones.
-                            Zone z = map.getZoneByID(map.getZoneSelected());
-
+                            Zone z = map.getZone(0);
+                            if(!showForm) {
+                                z = map.getZoneByID(map.getZoneSelected());
+                            }
                             Color colorNeutral = new Color(200, 200, 200, 0.6f);
 
                             int numberInfantry = 0, numberCavalry = 0;
@@ -146,64 +146,68 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
                             // Parmi toutes les unités de la zone d'origine
                             for (int in = 0; in < z.getUnits().size(); in++) {
                                 Unit unitDetail = z.getUnit(in);
-                                if(unitDetail.getClass().getSimpleName().equals("Infantry")){
+                                if (unitDetail.getClass().getSimpleName().equals("Infantry")) {
                                     numberInfantry++; // On récupère le nombre d'infanterie
-                                } else if (unitDetail.getClass().getSimpleName().equals("Cavalry")){
+                                } else if (unitDetail.getClass().getSimpleName().equals("Cavalry")) {
                                     numberCavalry++; // On récupère le nombre de cavalerie
                                 }
                             }
 
+                            showForm = true;
+
                             // On récupère le skin qu'on veut donner à notre formulaire (android.assets/ui/defaultskin.json)
-                            Skin skin = new Skin( Gdx.files.internal( "ui/defaultskin.json" ));
+                            Skin skin = new Skin(Gdx.files.internal("ui/defaultskin.json"));
 
                             final Table table = new Table(); // On créé une table pour mettre nos éléments du formulaire
                             table.setVisible(true); // Visible à vrai (ici c'est juste un test, par défaut c'est vrai)
                             table.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); // On met la taille de la table à celle de l'écran
-                            Gdx.app.log("GameScreen", "W = " + Gdx.graphics.getWidth() + " || H = " + Gdx.graphics.getHeight());
+
                             camera.zoom = 1.0f; // Faire un zoom lorsqu'on affiche le formulaire et le placer correctement par rapport à l'écran ? Désactiver le scroll et zoom lorsque le formulaire est affiché ?
 
                             // Pixmap ? C'est une bonne question ^
                             Pixmap pm1 = new Pixmap(1, 1, Pixmap.Format.RGB565);
-                            pm1.setColor(Color.GREEN);
+                            pm1.setColor(new Color(0f, 0f, 0f, 0.1f));
                             pm1.fill();
                             table.setBackground(new TextureRegionDrawable(new TextureRegion(new Texture(pm1))));
 
                             // Le label Infanterie
-                            Label labelInfantry=new Label("Infanterie",skin);
+                            Label labelInfantry = new Label("Infanterie", skin);
                             table.add(labelInfantry).width(150).padTop(10).padBottom(3);
 
                             // Le champ text pour le nombre d'infanterie à déplacer (trouver le moyen pour afficher un keyboard numeric)
-                            TextField textInfantry=new TextField("",skin);
+                            TextField textInfantry = new TextField("", skin);
                             table.add(textInfantry).width(200).height(50);
 
                             // Nombre total d'infanterie sur la zone
-                            Label totalInfantry=new Label("/" + numberInfantry, skin);
+                            Label totalInfantry = new Label("/" + numberInfantry, skin);
                             table.add(totalInfantry).width(50).height(50);
 
                             table.row(); // On revient à la ligne dans le tableau
 
                             // Le label Cavalerie
-                            Label labelCavalry=new Label("Cavalerie",skin);
+                            Label labelCavalry = new Label("Cavalerie", skin);
                             table.add(labelCavalry).width(150).padTop(10).padBottom(3);
 
                             // Le champ text pour le nombre de cavalerie à déplacer (trouver le moyen pour afficher un keyboard numeric)
-                            TextField textCavalry=new TextField("",skin);
+                            TextField textCavalry = new TextField("", skin);
                             table.add(textCavalry).width(200).height(50);
 
                             // Nombre total d'infanterie sur la zone
-                            Label totalCavalry=new Label("/" + numberCavalry, skin);
+                            Label totalCavalry = new Label("/" + numberCavalry, skin);
                             table.add(totalCavalry).width(50).height(50);
 
                             table.row(); // On revient à la ligne dans le tableau
 
                             // Le bouton valider
-                            TextButton valid=new TextButton("Valider",skin);
-                            valid.addListener(new ClickListener(){
+                            TextButton valid = new TextButton("Valider", skin);
+                            valid.addListener(new ClickListener() {
                                 @Override
                                 public void clicked(InputEvent event, float x, float y) {
-                                    table.addAction(Actions.fadeOut(0.7f));
                                     table.remove();
                                     camera.zoom = 2.0f;
+                                    showForm = false;
+                                    validForm = true;
+                                    Gdx.input.setOnscreenKeyboardVisible(false);
                                 }
                             });
 
@@ -211,56 +215,72 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
 
                             // On ajoute le formulaire au stage
                             stage.addActor(table);
+                            Gdx.app.log("GameScreen", "validForm = " + validForm);
 
+                            // Si le formulaire a été validé
+                            if (validForm) {
+                                validForm = false;
+                                Gdx.app.log("GameScreen", "Code ?");
+                                // Si on rencontre une zone sous le control du joueur (pour déplacer ses troupes) ou neutre (pour acquerir)
+                                if ((zone.getColor() == z.getColor()) || (zone.getColor().equals(colorNeutral))) {
 
+                                    // Pour le test on fait une liste d'unités à déplacer
+                                    ArrayList<Unit> unitsToMove = new ArrayList<Unit>();
+                                    Infantry infantry = new Infantry();
 
+                                    unitsToMove.addAll(zone.getUnits()); // On récupère toutes les unités de la zone selectionné
+                                    unitsToMove.add(infantry); // On ajoute nos unités à déplacer
 
-                            // Si on rencontre une zone sous le control du joueur (pour déplacer ses troupes) ou neutre (pour acquerir)
-                            if ((zone.getColor() == z.getColor()) || (zone.getColor().equals(colorNeutral))) {
+                                    ArrayList<Unit> totalUnitsZone = new ArrayList<Unit>();
 
-                                // Pour le test on fait une liste d'unités à déplacer
-                                ArrayList<Unit> unitsToMove = new ArrayList<Unit>();
-                                Infantry infantry = new Infantry();
+                                    // On récupère les unités dans la zone d'origine
+                                    totalUnitsZone = z.getUnits();
+                                    Unit unitToRemove = new Infantry(); // L'unité à supprimer
 
-                                unitsToMove.addAll(zone.getUnits()); // On récupère toutes les unités de la zone selectionné
-                                unitsToMove.add(infantry); // On ajoute nos unités à déplacer
-
-                                ArrayList<Unit> totalUnitsZone = new ArrayList<Unit>();
-
-                                // On récupère les unités dans la zone d'origine
-                                totalUnitsZone = z.getUnits();
-                                Unit unitToRemove = new Infantry(); // L'unité à supprimer
-
-                                int totalUnitStay = z.getUnits().size() - 1;
-                                Color originColor = z.getColor();
-                                if(totalUnitStay == 0){
-                                    z.setColor(colorNeutral);
-                                }
-
-                                // Parmi toutes les unités de la zone d'origine
-                                for (int in = 0; in < z.getUnits().size(); in++) {
-                                    unitToRemove = z.getUnit(in);
-                                    // Si la class d'une des unité de la zone d'origine est égale à la class de l'unité qui part (ici une infanterie en test)
-                                    if(unitToRemove.getClass().equals(infantry.getClass())){
-                                        break;
+                                    int totalUnitStay = z.getUnits().size() - 1;
+                                    Color originColor = z.getColor();
+                                    if (totalUnitStay == 0) {
+                                        z.setColor(colorNeutral);
                                     }
+
+
+                                    int inf = 0, cav = 0;
+
+                                    // Parmi toutes les unités de la zone d'origine
+                                    for (int in = 0; in < z.getUnits().size(); in++) {
+                                        unitToRemove = z.getUnit(in);
+                                        // Si la class d'une des unité de la zone d'origine est égale à la class de l'unité qui part (ici une infanterie en test)
+                                        if (unitToRemove.getClass().getSimpleName().equals("Infantry")) {
+                                            if (inf == Integer.valueOf(textInfantry.getText().toString())) {
+                                                continue;
+                                            }
+                                            inf++;
+                                            totalUnitsZone.remove(unitToRemove);
+                                        } else if (unitsToMove.getClass().getSimpleName().equals("Cavalry")) {
+                                            if (cav == Integer.valueOf(textCavalry.getText().toString())) {
+                                                continue;
+                                            }
+                                            cav++;
+                                            totalUnitsZone.remove(unitToRemove);
+                                        }
+                                    }
+
+                                    //totalUnitsZone.remove(unitToRemove); // Nombre d'unité restante
+
+                                    z.setUnits(totalUnitsZone);
+                                    zone.setUnits(unitsToMove);
+                                    zone.setColor(originColor);
+                                    zone.setDefaultColor(zone.getColor());
+                                    z.setActive(false);
+                                    z.setSelected(false);
+                                    zone.setActive(false);
+                                    zone.setSelected(false);
+                                    map.desactiveZones();
+                                    map.setZoneSelected(0);
+
+                                } else { // Sinon on rencontre un joueur adverse (d'une autre couleur donc) : on peut donc l'attaquer
+
                                 }
-
-                                totalUnitsZone.remove(unitToRemove); // Nombre d'unité restante
-
-                                z.setUnits(totalUnitsZone);
-                                zone.setUnits(unitsToMove);
-                                zone.setColor(originColor);
-                                zone.setDefaultColor(zone.getColor());
-                                z.setActive(false);
-                                z.setSelected(false);
-                                zone.setActive(false);
-                                zone.setSelected(false);
-                                map.desactiveZones();
-                                map.setZoneSelected(0);
-
-                            } else { // Sinon on rencontre un joueur adverse (d'une autre couleur donc) : on peut donc l'attaquer
-
                             }
                         }
                     }
@@ -406,8 +426,6 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
             newY = map.getSizey() - camera.viewportHeight/2*camera.zoom;
         if (newY - camera.viewportHeight/2*camera.zoom < 0)
             newY = 0 + camera.viewportHeight/2*camera.zoom;
-
-        Gdx.app.log("GameScreen", "Camera Width = " + camera.viewportWidth + " || camera height = " + camera.viewportHeight + " || Screen Width = " + Gdx.graphics.getWidth() + " || Screen Height = " + Gdx.graphics.getHeight());
 
         camera.position.x = newX;
         camera.position.y = newY;
